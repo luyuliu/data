4e1cbb1d1ca102f94e80eedf2ce2a05eb36c2400
Luyu Liu <liuluyu0378@gmail.com>
2021-11-22 15:45:56 -0500

    Update find_real_time.py

--- gtfs_realtime/find_real_time.py
+++ gtfs_realtime/find_real_time.py
@@ -1,14 +1,19 @@
+
 '''
 This script will find the real-time arrival/departure time from the GTFS
 real-time feed. 
 '''
 
-
-
 import pymongo
 from datetime import timedelta, date
 import time
 import multiprocessing
+
+import os
+import sys
+import time
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import transfer_tools
 
 def convertSeconds(BTimeString):
     time = BTimeString.split(":")
@@ -17,133 +22,211 @@ def convertSeconds(BTimeString):
     seconds = int(time[2])
     return hours * 3600 + minutes * 60 + seconds
 
+
 # database setup
 client = pymongo.MongoClient('mongodb://localhost:27017/')
 db_GTFS = client.cota_gtfs
 
-db_tripupdate=client.cota_tripupdate
+db_tripupdate = client.trip_update
 
-db_realtime=client.cota_real_time
-
-db_time_stamps_set=set()
-db_time_stamps=[]
-raw_stamps=db_GTFS.collection_names()
-for each_raw in raw_stamps:
-    each_raw=int(each_raw.split("_")[0])
-    db_time_stamps_set.add(each_raw)
-
-for each_raw in db_time_stamps_set:
-    db_time_stamps.append(each_raw)
-db_time_stamps.sort()
-
-
-# date setup
-
-
-def daterange(start_date, end_date):
-    for n in range(int((end_date - start_date).days)):
-        yield start_date + timedelta(n)
-
-def find_gtfs_time_stamp(today_date,single_date): # Find the corresponding GTFS set.
-    today_seconds=int((single_date - date(1970, 1, 1)).total_seconds()) + 18000
-    backup=db_time_stamps[0]
-    for each_time_stamp in db_time_stamps:
-        if each_time_stamp - today_seconds> 86400:
-            return backup
-        backup=each_time_stamp
-    return db_time_stamps[len(db_time_stamps)-1]
-
+db_realtime = client.cota_real_time
 
 
 # main loop
 # enumerate every day in the range
 def paralleling_transfers(single_date):
-    time_matrix={} # A dictionary: 1st layer: trip_id (each trip has a sequence of stops)
-                                #  2nd layer: stop_id (each stop in the sequence)
-    start_time=time.time()
+    # A dictionary: 1st layer: trip_id (each trip has a sequence of stops)
+    time_matrix = {}
+    #  2nd layer: stop_id (each stop in the sequence)
+    start_time = time.time()
     today_date = single_date.strftime("%Y%m%d")  # date
-    
-    db_feed_collection=db_tripupdate[today_date] # The GTFS feed collection which has been divided by each day.
-    
 
-    that_time_stamp=find_gtfs_time_stamp(today_date,single_date) # Find the corresponding GTFS set.
-    print("-----------------------",today_date,": Start/ From",date.fromtimestamp(that_time_stamp)," -----------------------")
+    # The GTFS feed collection which has been divided by each day.
+    col_feed = db_tripupdate[today_date]
+
+    # Find the corresponding GTFS set.
+    that_time_stamp = transfer_tools.find_gtfs_time_stamp(single_date)
+    print("-----------------------", today_date, ": Start/ From",
+          date.fromtimestamp(that_time_stamp), " -----------------------")
 
     # a=date.fromtimestamp(that_time_stamp)
     # print(a)
-    db_seq=db_GTFS[str(that_time_stamp)+"_trip_seq"]
-    db_stops=db_GTFS[str(that_time_stamp)+"_stops"]
-    db_stop_times=db_GTFS[str(that_time_stamp)+"_stop_times"]
-    db_trips=db_GTFS[str(that_time_stamp)+"_trips"]
+    db_seq = db_GTFS[str(that_time_stamp)+"_trip_seq"]
+    db_stops = db_GTFS[str(that_time_stamp)+"_stops"]
+    db_stop_times = db_GTFS[str(that_time_stamp)+"_stop_times"]
+    db_trips = db_GTFS[str(that_time_stamp)+"_trips"]
 
-    db_realtime_collection=db_realtime["R"+today_date] # Collection to be added.
-    db_feeds=(db_feed_collection.find({}, no_cursor_timeout=True)) # The GTFS feed collection which has been divided by each day.
-    if db_feeds.count()==0: # There is no feed this day, which shouldn't happen.
-        print("-----------------------",today_date," : Skip -----------------------")
+    # Collection to be added.
+    col_real_time = db_realtime["R"+today_date]
+    # The GTFS feed collection which has been divided by each day.
+    rl_feeds = (col_feed.find({}, no_cursor_timeout=True))
+    if rl_feeds.count() == 0:  # There is no feed this day, which shouldn't happen.
+        print("-----------------------", today_date,
+              " : Skip -----------------------")
         return
-    print("-----------------------","FindDone","-----------------------")
-    total_count=db_feeds.count() # Total length of this day's feed.
+    print("-----------------------", "FindDone", "-----------------------")
+    total_count = rl_feeds.count()  # Total length of this day's feed.
 
-    count=0
-    for each_feed in db_feeds: # For each feed record.
-        trip_id=each_feed["trip_id"] # trip_id
-        seq_count=0 # Sequence's count. It records the sequence id of different records of the same trip at different time.
-        # For example, if a bus/trip run on a route, the system will send back several different GTFS real-time reports at each time. 
+    count = 0
+    for each_feed in rl_feeds:  # For each feed record.
+        trip_id = each_feed["trip_id"]  # trip_id
+        # Sequence's count. It records the sequence id of different records of the same trip at different time.
+        seq_count = 0
+        # For example, if a bus/trip run on a route, the system will send back several different GTFS real-time reports at each time.
 
         # We always assume the lastest record is the most accurate one. So, we have to store the seq_count to compare the current one and the new one
         # to determine whether we should update the records.
 
-        # We will select the smallest count. This is because with the bus running, the seq in its GTFS real-time will become shorter. If there's a smaller count 
+        # We will select the smallest count. This is because with the bus running, the seq in its GTFS real-time will become shorter. If there's a smaller count
         # then it means that the current record is after the best record we record before.
 
-        for each_stop in each_feed["seq"]: # The sequence of each feed record.
-            stop_id=each_stop["stop"] # stop_id of each sequential stop.
+        for each_stop in each_feed["seq"]:  # The sequence of each feed record.
+            stop_id = each_stop["stop"]  # stop_id of each sequential stop.
             try:
-                time_matrix[trip_id] # Create time_matrix's first layer: trip_id. Each trip should has a unique records in the first layer.
+                # Create time_matrix's first layer: trip_id. Each trip should has a unique records in the first layer.
+                time_matrix[trip_id]
             except:
-                time_matrix[trip_id]={}
+                time_matrix[trip_id] = {}
             else:
                 pass
             try:
                 time_matrix[trip_id][stop_id]
-            except: # if the stop is not recorded, then create it and add it to the trip_id.
-                time_matrix[trip_id][stop_id]={}
-                time_matrix[trip_id][stop_id]={"seq":seq_count,"time":each_stop["arr"]}
+            except:  # if the stop is not recorded, then create it and add it to the trip_id.
+                time_matrix[trip_id][stop_id] = {}
+                time_matrix[trip_id][stop_id] = {
+                    "seq": seq_count, "time": each_stop["arr"]}
             else:
-                if time_matrix[trip_id][stop_id]["seq"]>seq_count: # If the current seq_count is less, then update the records with the newest feed.
-                    time_matrix[trip_id][stop_id]={"seq":seq_count,"time":each_stop["arr"]}
-            seq_count+=1
-        
-        if count%10000==0:
-            print("-----------------------","QueryDoneBy:",count/total_count*100,today_date,"-----------------------")
-        count+=1
-    
-    for trip_id in time_matrix.keys(): # trip_id
+                # If the current seq_count is less, then update the records with the newest feed.
+                if time_matrix[trip_id][stop_id]["seq"] > seq_count:
+                    time_matrix[trip_id][stop_id] = {
+                        "seq": seq_count, "time": each_stop["arr"]}
+            seq_count += 1
+
+        if count % 10000 == 0:
+            print("-----------------------", "QueryDoneBy:", count /
+                  total_count*100, today_date, "-----------------------")
+        count += 1
+
+    today_date = single_date.strftime("%Y%m%d")  # date
+    today_seconds = int(time.mktime(time.strptime(today_date, "%Y%m%d")))
+    today_weekday = single_date.weekday()  # day of week
+    if today_weekday < 5:
+        service_id = 1
+    elif today_weekday == 5:
+        service_id = 2
+    else:
+        service_id = 3
+
+    print(today_date, ": Start.")
+    stop_dic = {}
+    trip_dic = {}
+    count = 0
+
+    insertionList = []
+    for trip_id in time_matrix.keys():  # trip_id
         for stop_id in time_matrix[trip_id].keys():
-            recordss={}
-            recordss["trip_id"]=trip_id
-            recordss["stop_id"]=stop_id
-            recordss["seq"]=time_matrix[trip_id][stop_id]["seq"] # This seq is different from the seq in the trip_seq.
-            recordss["time"]=time_matrix[trip_id][stop_id]["time"]
-            db_realtime_collection.insert_one(recordss)
-    print("-----------------------","InsertDone:",today_date,"-----------------------")
-    end_time=time.time()
+            recordss = {}
+            recordss["trip_id"] = trip_id
+            recordss["stop_id"] = stop_id
+            # This seq is different from the seq in the trip_seq.
+            recordss["seq"] = time_matrix[trip_id][stop_id]["seq"]
+            recordss["time"] = time_matrix[trip_id][stop_id]["time"]
+            
+            ################## Do join in advance ################
+            try:
+                stop_dic[stop_id]
+            except:
+                stop_dic[stop_id] = {}
+                stop_query = (db_stops.find_one({"stop_id": stop_id}))
+                if (stop_query) == None:
+                    stop_dic[stop_id]["lat"] = "stop_error"
+                    stop_dic[stop_id]["lon"] = "stop_error"
+                    stop_dic[stop_id]["stop_name"] = "stop_error"
+                    stop_dic[stop_id]["stop_code"] = "stop_error"
+                else:
+                    stop_dic[stop_id]["lat"] = stop_query["stop_lat"]
+                    stop_dic[stop_id]["lon"] = stop_query["stop_lon"]
+                    stop_dic[stop_id]["stop_name"] = stop_query["stop_name"]
+                    stop_dic[stop_id]["stop_code"] = stop_query["stop_code"]
+
+            try:
+                trip_dic[trip_id]
+            except:
+                trip_dic[trip_id] = {}
+                trip_query = (db_trips.find_one({"trip_id": trip_id}))
+                if (trip_query) == None:
+                    trip_dic[trip_id]['block_id'] = "trip_error"
+                    trip_dic[trip_id]['shape_id'] = "trip_error"
+                    trip_dic[trip_id]['trip_headsign'] = "trip_error"
+                    trip_dic[trip_id]['direction_id'] = "trip_error"
+                    trip_dic[trip_id]['route_id'] = "trip_error"
+                else:
+                    trip_dic[trip_id]['block_id'] = trip_query["block_id"]
+                    trip_dic[trip_id]['shape_id'] = trip_query["shape_id"]
+                    trip_dic[trip_id]['trip_headsign'] = trip_query["trip_headsign"]
+                    trip_dic[trip_id]['direction_id'] = int(
+                        trip_query["direction_id"])
+                    trip_dic[trip_id]['route_id'] = int(
+                        trip_query["route_id"])*(1-2*int(trip_query["direction_id"]))  # 0->1; 1->-1
+
+            stop_times_query = (db_stop_times.find_one(
+                {"trip_id": trip_id, "stop_id": stop_id}))
+            if (stop_times_query) == None:
+                stop_sequence = "stop_time_error"
+            else:
+                stop_sequence = stop_times_query["stop_sequence"]
+
+            trip_seq_query = (db_seq.find_one(
+                {"trip_id": trip_id, "stop_id": stop_id}))
+            if (trip_seq_query) == None:
+                trip_sequence = "GTFS_error"
+                scheduled_time = "GTFS_error"
+            else:
+                trip_sequence = trip_seq_query["seq"]
+                scheduled_time = int(trip_seq_query["time"]) + today_seconds
+
+            recordss["lat"] = stop_dic[stop_id]["lat"]
+            recordss["lon"] = stop_dic[stop_id]["lon"]
+            recordss["stop_name"] = stop_dic[stop_id]["stop_name"]
+            recordss["shape_id"] = trip_dic[trip_id]['shape_id']
+            recordss["trip_headsign"] = trip_dic[trip_id]['trip_headsign']
+            recordss["route_id"] = trip_dic[trip_id]['route_id']
+            recordss["stop_sequence"] = stop_sequence
+            recordss["trip_sequence"] = trip_sequence
+            recordss["scheduled_time"] = scheduled_time
+
+            count += 1
+            if count % 10000 == 1:
+                print(today_date, ": ", count/total_count*100)
+
+            insertionList.append(recordss)
+
+            if len(insertionList) == 10000:
+                col_real_time.insert_many(insertionList)
+                insertionList = []
+
+    col_real_time.insert_many(insertionList)
+    insertionList = []
+
+    print("-----------------------", "InsertDone:", today_date, "-----------------------")
+    end_time = time.time()
     print(end_time-start_time)
-    db_realtime_collection.create_index([("trip_id",1),("stop_id",1)])
+
+    col_real_time.create_index([("trip_id", 1), ("stop_id", 1)])
 
 
 if __name__ == '__main__':
-    start_date = date(2018, 9, 3)
-    end_date = date(2019, 1, 31)
-    each_date = date(2018, 2, 1)
-    paralleling_transfers(each_date)
+    start_date = date(2019, 1, 31)
+    end_date = date(2019, 6, 20)
+    # each_date = date(2018, 1, 29)
+    # paralleling_transfers(each_date)
     ''' for each_date in daterange(start_date, end_date):
         paralleling_transfers(each_date)'''
 
-    # cores = multiprocessing.cpu_count()
-    # pool = multiprocessing.Pool(processes=20)
-    # date_range = daterange(start_date, end_date)
-    # output=[]
-    # output=pool.map(paralleling_transfers, date_range)
-    # pool.close()
-    # pool.join()
+    cores = multiprocessing.cpu_count()
+    pool = multiprocessing.Pool(processes=30)
+    date_range = transfer_tools.daterange(start_date, end_date)
+    output=[]
+    output=pool.map(paralleling_transfers, date_range)
+    pool.close()
+    pool.join()
